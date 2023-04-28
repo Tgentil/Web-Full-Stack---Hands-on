e4f5244954d6de87be161db0cab30d6877ac6d26
Thiago Gentil <thiagosgentil@gmail.com>
2023-04-28 11:33:19 -0300

    first commit

---
+++ src/components/CardContato.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <q-card
+      class="my-card text-white q-ma-md"
+      style="background: radial-gradient(circle, #35a2ff 0%, #014a88 100%)"
+    >
+      <q-card-section>
+        <div class="text-h6">{{ contato.nome }}</div>
+        <div class="text-subtitle2">{{ contato.email }}</div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        <h6>{{ contato.fone }}</h6>
+      </q-card-section>
+    </q-card>
+    <!--
+      <input v-model="contato.fone"/>
+-->
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+export default defineComponent({
+  props: {
+    contato: Object,
+  },
+});
+</script>
+
+<style lang="sass" scoped>
+.my-card
+  width: 100%
+  max-width: 250px
+</style>
---
+++ src/pages/NossaPagina.vue
@@ -0,0 +1,75 @@
+<template>
+  <q-layout view="hHr lpR fFf">
+    <q-header bordered class="bg-primary text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
+          </q-avatar>
+          Title
+        </q-toolbar-title>
+
+        <q-btn dense flat round icon="menu" @click="toggleRightDrawer" />
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      show-if-above
+      v-model="rightDrawerOpen"
+      side="right"
+      behavior="desktop"
+      elevated
+    >
+      <!-- drawer content -->
+    </q-drawer>
+
+    <q-page-container>
+      <button @click="add">+</button>
+      <input v-model="arrContatos[0].nome" />
+      <div v-for="c in arrContatos" v-bind:key="c.fone">
+        <card-contato :contato="c" />
+      </div>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { ref } from "vue";
+import CardContato from "../components/CardContato.vue";
+
+export default {
+  components: { CardContato },
+  methods: {
+    add: function () {
+      this.arrContatos.push({
+        nome: "Recruta",
+        sobrenome: "Madagasgar",
+        fone: "00000",
+        email: "recruta@penguins.com",
+      });
+    },
+  },
+  data() {
+    return {
+      arrContatos: [
+        {
+          nome: "Thiago Gentil",
+          sobrenome: "Gentil",
+          fone: "48 99998888",
+          email: "thiagogentil@fakegmail.com",
+        },
+      ],
+    };
+  },
+  setup() {
+    const rightDrawerOpen = ref(false);
+
+    return {
+      rightDrawerOpen,
+      toggleRightDrawer() {
+        rightDrawerOpen.value = !rightDrawerOpen.value;
+      },
+    };
+  },
+};
+</script>
